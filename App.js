60b1c999e75e2f7c8882d0b2e2740814d9637fbd
Jack Bayliss <jack@jackbayliss.com>
2020-07-13 00:03:12 +0100

    Sample app init
    Initial sample app release.

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import {View, Button, Text} from 'react-native'
+import NfcCardReader from 'react-native-nfc-card-reader'
+
+
+
+export default class App extends Component {
+
+
+  state = {
+    cardNumber : null,
+    expiryDate : null,
+    cardType : null,
+
+ }
+
+
+ async startScan(){
+   let that = this;
+   await NfcCardReader.startNfc(function(cardDetails){
+     console.log(cardDetails)
+     that.setState({cardNumber : cardDetails.cardNumber})
+     that.setState({expiryDate : cardDetails.expiryDate})
+     that.setState({cardType : cardDetails.cardType})
+
+    })
+ }
+
+  render() {
+  return <View style={{flex:1,justifyContent: 'center',margin:20, alignContent:'center'}}>
+    <Button title="Start NFC Scan!" onPress={() => {this.startScan()}} ></Button>
+    <Text>{this.state.cardNumber}</Text>
+    <Text>{this.state.expiryDate}</Text>
+    <Text>{this.state.cardType}</Text>
+
+  </View>
+
+   
+  }
+}
